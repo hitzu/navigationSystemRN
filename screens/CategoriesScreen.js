c1307875a6049ad569a2a555eb9b28c2152330ce
hitzu <robert.torres.lopez@gmail.com>
2020-03-12 12:30:00 -0600

    [Update] (navigation) pase de parametros entre componentes

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -1,22 +1,54 @@
 import React from 'react';
-import { View, Text, StyleSheet, Button } from 'react-native';
+import { View, Text, StyleSheet, Button, FlatList, TouchableOpacity } from 'react-native';
+// import {  } from 'react-native-gesture-handler';
+
+import { CATEGORIES } from '../data/dummy-data';
+import Colors from '../constant/Colors';
 
 const CategoriesScreen = props => {
+
+  console.log(Colors)
+
+  const renderGridItem = (itemData) => {
+    return (
+        <TouchableOpacity style = { styles.gridItem } onPress = { () => {
+          props.navigation.navigate({
+            routeName : 'CategoryMeals',
+            params : {
+              categoryId : itemData.item.id
+            }
+          })
+        } }>
+          <View>
+            <Text>{itemData.item.title}</Text>
+          </View>
+        </TouchableOpacity>
+      )
+  }
+
   return (
-    <View style={styles.screen}>
-      <Text>The Categories Screen!</Text>
-      <Button title = "Go to Meals" onPress={ () => {
-        props.navigation.navigate({routeName : "CategoryMeals"});
-      } } /> 
-    </View>
+    <FlatList data = {CATEGORIES} renderItem = {renderGridItem}  numColumns = {2} keyExtractor = {(item,index) => item.id} />
   );
 };
+
+CategoriesScreen.navigationOptions = {
+  headerTitle : 'Meal Categories',
+  headerStyle : {
+    backgroundColor : Colors.primaryColor
+  },
+  headerTintColor : 'white'
+}
 
 const styles = StyleSheet.create({
   screen: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center'
+  },
+  gridItem : {
+    flex : 1,
+    margin : 15,
+    height : 150
   }
 });
 
--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import { View, Text, StyleSheet, Button } from 'react-native';
 
+import {CATEGORIES} from '../data/dummy-data'
+
 const CategoryMealScreen = props => {
+
+  const catId = props.navigation.getParam('categoryId')
+  console.log(catId)
+  const selectedCategory = CATEGORIES.find(cat => cat.id === catId);
+
   return (
     <View style={styles.screen}>
       <Text>The Category Meal Screen!</Text>
+      <Text>{selectedCategory.title}</Text>
       <Button title = "Go to Details" onPress = { () => {
         props.navigation.navigate(
           {routeName : 'MealDetail'})
